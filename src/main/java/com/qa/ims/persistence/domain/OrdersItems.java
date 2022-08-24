5e36fbde37f7d2819d99aacef61918afe0c44950
IsabellaOHara <isabellaohara@btinternet.com>
2022-08-24 12:53:15 +0100

    adding multiple items to one order functionality

--- src/main/java/com/qa/ims/persistence/domain/OrdersItems.java
+++ src/main/java/com/qa/ims/persistence/domain/OrdersItems.java
@@ -1,5 +1,8 @@
 package com.qa.ims.persistence.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 public class OrdersItems {
@@ -8,14 +11,16 @@ public class OrdersItems {
 	private Long quantity;
 	private Long id;
 	private Long itemId;
-	
-	
 
 	public OrdersItems(Long id, Long itemId, Long quantity) {
 		super();
 		this.setId(id);
 		this.setItemId(itemId);
 		this.setQuantity(quantity);
+	}
+
+	public OrdersItems(List<OrdersItems> orderDetails) {
+		
 	}
 
 	public Long getQuantity() {
